c94c754b1a14b5dc79e38f6de81df4f65fbf16e5
Nilotpal Das <neelot3pal@gmail.com>
2020-10-12 23:39:16 +0530

    authentication done on post both in frontend and backend

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,24 @@
 
+const Post = require("../models/post");
+
 module.exports.home=function(req,res){
    
-        console.log(req.cookies);
-        res.cookie('user_id',25);
-        res.cookie('p',23);
-        return res.render('home',{
-            title: "Codecial"
+        // console.log(req.cookies);
+        // res.cookie('user_id',25);
+        // Post.find({},function(err,posts){
+        //     return res.render('home',{
+        //         title: "Codecial|Home",
+        //         posts: posts
+        //    });
+        // });
+
+        //populate the user of each post
+        Post.find({}).populate('user').exec(function(err,posts){
+            return res.render('home',{
+                title: "Codecial|Home",
+                posts: posts
         });
+
+    })
     
 }
